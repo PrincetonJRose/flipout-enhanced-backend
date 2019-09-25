35624ea2b303f368803a3c79fa93438d3773ec2a
github username <github email address>
2019-09-25 09:05:17 -0400

    initial setup of themes controller

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,6 +1,13 @@
 class ThemesController < ApplicationController
+
+    def index
+        @themes = Theme.all
+        render json: @themes, status: :ok
+    end
+
     def show
         @themes = Theme.where(name: params[:id])
         render json: @themes, status: :ok
     end
+    
 end
