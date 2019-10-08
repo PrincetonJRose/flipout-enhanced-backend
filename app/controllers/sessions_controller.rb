9369bde1256fd62be54856a764b7eef99c0ed87c
github username <github email address>
2019-10-08 13:03:44 -0400

    changed jwt token in sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
         @user = User.find_by(username: params[:username])
         if @user and @user.authenticate(params[:password])
             jwt_token = JWT.encode({ user_id: @user.id }, ENV["JWT_AUTH_KEY"], "HS256")
-            render json: { jwt_user: jwt_token }, status: :ok
+            render json: { jwt_token: jwt_token }, status: :ok
         else
             render json: { errors: ["Invalid email and/or password."] }, status: :unauthorized
         end
